7c9815b1f62dd6c5d6900c77e9460b8261990a45
Jesse Barron <jessebarron1113@gmail.com>
2018-04-11 12:47:07 -0500

    implemented fetch-next-page

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// require('./secrets/secrets')
+require('./secrets/secrets')
 const feathers = require('@feathersjs/feathers')
 const express = require('@feathersjs/express')
 const socketio = require('@feathersjs/socketio')
@@ -19,64 +19,29 @@ app.configure(socketio())
 app.use(bodyParser.json())
 
 
-class MyMessages {
-    constructor() {
-        this.messages = []
-        this.currentId = 0
-    }
-    
-    async find(params) {
-        return this.messages
-    }
-    
-    async get(id, params) {
-        const message = this.messages.find(message => message.id === parseInt(id, 10))
-
-        if (!message) throw new Error('This message does not exist')
-        return message
-    }
-    
-    async create(data, params) {
-        const message = Object.assign({id: ++this.currentId}, data)
-        this.messages.push(message)
-        return message
-    }
-
-    async patch(id, data, params) {
-        const message = await this.get(id)
-
-        message = Object.assign(message, data)
-    }
-
-    async remove(id, params) {
-        const message = await this.get(id)
-        const index = this.messages.indexOf(message)
-        this.messages.splice(index, 1)
-
-        return message
-    }
-}
-
-
 app.use(express.errorHandler())
 
 const server = app.listen(PORT)
 
-app.use('messages', new MyMessages)
 app.use('feed', new FacebookFeed)
 app.use('streamInfo', new VoxStreamInfo)
 
 
 app.on('connection', connection => app.channel('everybody').join(connection))
 app.publish(() => app.channel('everybody'))
-server.on('listening', () => console.log('Server is listening in port 8080'))
-app.service('messages').on('created', message => console.log(`New Message ${message.id}: ${message.text}`))
+server.on('listening', () => console.log(`Server is listening in port ${PORT}`))
 app.service('streamInfo').on('updated', currentSong => {
     console.log(`Song Changed to: ${currentSong}`)
     return currentSong
 })
 
-
+// app.service('feed').find()
+//     .then(res => {
+//        return app.service('feed').fetchNextPage(res.nextPage)
+//     })
+//     .then(moreFeed => {
+//         console.log(moreFeed.feed)
+//     })
 
 poller()
 
